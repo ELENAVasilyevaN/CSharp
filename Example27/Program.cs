655d226517413d8ae76c5e7c099e54e9d28c8697
«Elena <tetlada@mail.ru>
2023-02-11 11:13:22 +0300

    Zadacha27

---
+++ Example27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int SumNum(int Num)
+{
+    int res = 0;
+    while (Num > 0)
+    {
+        res = res + Num % 10;
+        Num = Num / 10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число Num: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в числе {Num} равна {SumNum(Num)}");
